5ca793ab0ba66de0e5b1baff33e313abd88d8a28
DESKTOP-4ALDHGC\박태현 <ithpark1035@gmail.com>
2024-05-23 10:44:09 +0900

    qovh

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from'react-router-dom';
+import { BrowserRouter, HashRouter } from'react-router-dom';
 import ScrollToTop from './common/ScrollToTop';
 import './scss/main.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ScrollToTop/>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <HashRouter>
+    <React.StrictMode>
+        <ScrollToTop/>
+        <App />
+    </React.StrictMode>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
